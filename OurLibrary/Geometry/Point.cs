d9c70d3e9cf80abb3b70c793d6306b95f5903593
nrmontagne <nicolas.montagne@enpc.fr>
2022-03-30 17:13:16 +0200

    Initialise Project Library

---
+++ OurLibrary/Geometry/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LabLibrary
+{
+    /// <summary>
+    /// Class defining a point in 3D euclidean space
+    /// </summary>
+    public class Point
+    {
+        /// <summary>
+        /// First coordinate of the point.
+        /// </summary>
+        public double X { get; set; }
+        /// <summary>
+        /// Second coordinate of the point.
+        /// </summary>
+        public double Y { get; set; }
+        /// <summary>
+        /// Third coordinate of the point.
+        /// </summary>
+        public double Z { get; set; }
+
+        /// <summary>
+        /// Initialises a new <see cref="Point"/> class.
+        /// </summary>
+        /// <param name="x"> First coordinate of the point. </param>
+        /// <param name="y"> Second coordinate of the point. </param>
+        /// <param name="z"> Third coordinate of the point. </param>
+        public Point(double x, double y, double z)
+        {
+            X = x; Y = y; Z = z;
+        }
+    }
+}
---
+++ OurLibrary/Numerics/Complex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurLibrary.Numerics
+{
+    public class Complex
+    {
+        double Real;
+
+        double Imaginary;
+    }
+}
